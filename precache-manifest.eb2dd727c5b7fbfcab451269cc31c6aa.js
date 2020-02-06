85fc6444c88783dca4c6327748c683e46b63cdc8
Travis CI <aleodoni@gmail.com>
2020-02-06 10:53:18 +0000

    Deploy to GitHub Pages

---
+++ precache-manifest.eb2dd727c5b7fbfcab451269cc31c6aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75c68f60cf246c643c8f9e82f8cbc01f",
+    "url": "/cafe-dif/index.html"
+  },
+  {
+    "revision": "d86a6550bfeaceecc345",
+    "url": "/cafe-dif/static/js/2.8a649cc7.chunk.js"
+  },
+  {
+    "revision": "1738ce7df2e8ff103930",
+    "url": "/cafe-dif/static/js/main.3b631a4b.chunk.js"
+  },
+  {
+    "revision": "ed994da93b7164e9f8b0",
+    "url": "/cafe-dif/static/js/runtime-main.26e044af.js"
+  },
+  {
+    "revision": "8f53a40f1f5ac9e779db9564d916c3dc",
+    "url": "/cafe-dif/static/media/melitta1.8f53a40f.png"
+  },
+  {
+    "revision": "9efa03054d0c97dc55848e7cfa475572",
+    "url": "/cafe-dif/static/media/melitta2.9efa0305.png"
+  }
+]);
